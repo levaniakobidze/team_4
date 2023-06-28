badd6b485e305dde8a01d57b0324ca4fde2d2360
anano303 <124179773+anano303@users.noreply.github.com>
2023-06-29 03:40:48 +0400

    feat:localstorage clear on board

--- src/components/onboarding-component/Board.jsx
+++ src/components/onboarding-component/Board.jsx
@@ -2,6 +2,9 @@ import "./Board.css";
 import Logo from "/src/assets/icons/Onboarding-completed.png";
 
 export default function board() {
+  useEffect(() => {
+    localStorage.clear(); // Clear local storage on component mount
+  }, []);
   return (
     <div className="lastPage">
       <img className="lastLogo" src={Logo} alt="logo" />
